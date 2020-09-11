60ca3e1d166b0dae452e7fd8d13131cc713a4d2c
Andrew Wright <andDevW@gmail.com>
2020-09-10 17:32:12 -0700

    Add script for conversion to AAC

---
+++ EasyAAC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# This script requires the installation of youtube-dl
+# In Debian # apt-get install youtube-dl
+# 
+
+read -p "Please enter the YouTube URL you want to convert to an MP3 and hit Enter : " url
+
+
+echo `youtube-dl --extract-audio --audio-format aac --audio-quality 0  $url`
+
